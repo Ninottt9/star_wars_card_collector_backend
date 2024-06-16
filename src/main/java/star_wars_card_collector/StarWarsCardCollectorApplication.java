4a4ac2db232cbfa4926d2cd35e220bd8bdb0c470
Ninottt9 <mkazhaev746@gmail.com>
2024-06-16 20:19:19 +0200

    cors policy added

--- src/main/java/star_wars_card_collector/StarWarsCardCollectorApplication.java
+++ src/main/java/star_wars_card_collector/StarWarsCardCollectorApplication.java
@@ -2,12 +2,27 @@ package star_wars_card_collector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class StarWarsCardCollectorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StarWarsCardCollectorApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**").allowedOrigins("*");
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
 }

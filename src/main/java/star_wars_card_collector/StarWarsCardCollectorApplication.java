f0180e1cf2a627926e9d12f0470af8ff9ba6e733
Ninottt9 <mkazhaev746@gmail.com>
2024-06-16 15:59:11 +0200

    first commit

---
+++ src/main/java/star_wars_card_collector/StarWarsCardCollectorApplication.java
@@ -0,0 +1,18 @@
+package star_wars_card_collector;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class StarWarsCardCollectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarWarsCardCollectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/star_wars_card_collector/StarWarsCardCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package star_wars_card_collector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarWarsCardCollectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
